1d6f6f69a509bf36fff8ac376fe1e22318635adc
RlYeli02 <franyelizarodriguez@live.com>
2022-05-04 20:09:07 -0400

    error page modified

--- CLIENT/commit_history_client/src/components/ErrorPage.js
+++ CLIENT/commit_history_client/src/components/ErrorPage.js
@@ -6,6 +6,7 @@ const ErrorPage = ({error})=>{
   return(
       <div className="error">
         <h1>{`${error.message}, -  ${error.code}`}</h1>
+        <h4>Refresh your page</h4>
       </div>
   )
 }
--- CLIENT/commit_history_client/src/pages/Commits.js
+++ CLIENT/commit_history_client/src/pages/Commits.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import axios from "axios";
 import Card from "../components/Card";
 import NavBar from "../components/NavBar";
@@ -6,24 +6,35 @@ import ErrorPage from "../components/ErrorPage"
 
 const CommitList = () => {
     const [commits, setCommits] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [token,setToken] = useState ()
 
     const getCommits = async () => {
         try {
-            let response = await axios.get('http://localhost:5000/github/commits/');
-            setCommits(response.data)
-            setLoading(false)
+            setLoading(true)
+          
+            
+                let response = await axios.get(`http://localhost:5000/github/commits/`,{
+                    headers:{
+                        'Authorization':`token ${token}`
+                    }
+                });
+                setCommits(response.data)
+                setLoading(false)
+            
         } catch (error) {
             setError(error)
             setLoading(false)
         }
     }
 
-    useEffect(() => {
-        getCommits()
-    }, [])
+    const onChange = (e)=>{
+        const {value}= e.target;
+        setToken(value)
+    }
 
+   
     if (loading) return "Loading..."
     if (error) {
         return (
@@ -33,6 +44,10 @@ const CommitList = () => {
     return (
         <div>
             <NavBar />
+            <div style={{marginLeft:"1rem"}}>
+            <input style={{width:"30rem", height:"1.5rem"}} type="text" onChange={onChange} placeholder="Enter the OAuth token given in the readmeFile.md" />
+            <button style={{fontFamily: 'Nunito', height:"1.5rem"}} onClick={getCommits}>Find Commit List</button>
+            </div>
             <Card commits={commits}/>
         </div>
     )
